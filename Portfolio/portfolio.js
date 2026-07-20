e4690a3e7ffd8eee6e6083ee0a150a17f622319a
| Priyanka saini 🚀..| <geetapriyanka90974@gmail.com>
2026-07-20 15:21:11 +0530

    Remove smooth scrolling functionality from navigation

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,7 +1,1 @@
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
+
